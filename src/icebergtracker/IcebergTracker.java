0796bd92aa7b66e8b716b53aa5eb91fcb6ab1dce
Gabriel <Gabriel@10.13.15.126>
2018-10-17 12:22:36 +0100

    IcebergTracker v2.1

--- src/icebergtracker/IcebergTracker.java
+++ src/icebergtracker/IcebergTracker.java
@@ -18,26 +18,14 @@ public class IcebergTracker {
         Iceberg iceberg1 = new Iceberg(1, "cabo ABC", LocalDate.of(2017, Month.MARCH, 1));
         Iceberg iceberg2 = new Iceberg(2, "bahía DEF", LocalDate.of(2017, Month.MARCH, 10));;
         
-        mapa.put(iceberg1, new IcebergData(
-                LocalDate.of(2018, Month.MARCH, 1), 
-                iceberg1, 
-                "80º N 15' E", 
-                9e9));
-        mapa.put(iceberg1, new IcebergData(
-                LocalDate.of(2018, Month.MARCH, 30), 
-                iceberg1, 
-                "80º N 12' E", 
-                8e9));
-        mapa.put(iceberg2, new IcebergData(
-                LocalDate.of(2018, Month.MARCH, 1), 
-                iceberg2, 
-                "88º S 15' W", 
-                7e9));
-        mapa.put(iceberg2, new IcebergData(
-                LocalDate.of(2018, Month.MARCH, 30), 
-                iceberg2, 
-                "88º S 30' W", 
-                6e9));
+        mapa.put(iceberg1, new IcebergData(LocalDate.of(2018, Month.MARCH, 1), 
+                iceberg1, "80º N 15' E", 9e9));
+        mapa.put(iceberg1, new IcebergData(LocalDate.of(2018, Month.MARCH, 30), 
+                iceberg1, "80º N 12' E", 8e9));
+        mapa.put(iceberg2, new IcebergData(LocalDate.of(2018, Month.MARCH, 1), 
+                iceberg2, "88º S 15' W", 7e9));
+        mapa.put(iceberg2, new IcebergData(LocalDate.of(2018, Month.MARCH, 30), 
+                iceberg2, "88º S 30' W", 6e9));
     }
     
     private void output() {
